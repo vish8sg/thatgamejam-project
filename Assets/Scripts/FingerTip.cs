f1ee61d88df347afa4e6ed972f2fe129ee1e1f81
vish8sg <tvishnu2008@gmail.com>
2025-12-27 17:32:10 +0800

    feeling a little better about the movement, will implement the visuals first to see if it feels any better, otherwise scrap

--- Assets/Scripts/FingerTip.cs
+++ Assets/Scripts/FingerTip.cs
@@ -6,22 +6,26 @@ using UnityEngine;
 public class FingerTip : MonoBehaviour
 {
 
+    /*pull out your notebook to figure out how you want this to work. What I want is for the control while anchored to be precise, but for momentum to be conserved so you can 
+    swing when you let go. Figure out how moving the hand invesrse the mouse position (within the constraints) applies to velocity, then to force, and apply it. 
+    */
+
     [SerializeField] Rigidbody2D palmRb;
     [SerializeField] float fingerLength;
     [SerializeField] float fingerSpeed;
-    [SerializeField] float pullStrength;
+    [SerializeField] float pullSoftness;
+    [SerializeField] float maxPullStrength;
     [Range(0,1)] [SerializeField] float palmVelocityDampening;
 
     bool isAnchored = false;
     Vector3 anchoredPosition = Vector3.zero;
     Vector3 mousePosition = Vector3.zero;
-    Vector3 prevMousePosition = Vector3.zero;
+    Vector3 anchoredMousePosition = Vector3.zero;
 
     private void Update()
     {
 
-        //getting target position and making sure it doesn't excede finger length
-        prevMousePosition = mousePosition;
+        //getting target position and making sure it doesn't excede finger 
         mousePosition = GetMouseWorldPosition();
         //Vector3 mouseDelta = mousePosition - prevMousePosition;
 
@@ -30,35 +34,16 @@ public class FingerTip : MonoBehaviour
         {
             isAnchored = true;
             anchoredPosition = transform.position;
+            anchoredMousePosition = mousePosition;
         }
         if (Input.GetMouseButtonUp(0))
         {
             isAnchored = false;
-        }    
-
-        if (isAnchored)
-        {
-            Vector2 toPalm = palmRb.position - (Vector2) anchoredPosition;
-            Vector2 mouseDelta = mousePosition - anchoredPosition;
-            Vector2 targetPosition = palmRb.position - mouseDelta;
-
-            Vector2 offsetFromAnchor = targetPosition - (Vector2) anchoredPosition;
-            if (offsetFromAnchor.magnitude > fingerLength)
-            {
-                offsetFromAnchor = offsetFromAnchor.normalized * fingerLength;
-            }
-
-            Vector2 clampedTargetPositon = (Vector2) anchoredPosition + offsetFromAnchor;
-
-            Vector2 correction = clampedTargetPositon - palmRb.position;
-            palmRb.velocity *= palmVelocityDampening; //optional drag to prevent buildup
-            palmRb.AddForce(correction * pullStrength, ForceMode2D.Force);
-
-            
         }
-        else
+
+        if (!isAnchored)
         {
-            Vector2 direction = (Vector2) mousePosition - palmRb.position;
+            Vector2 direction = (Vector2)mousePosition - palmRb.position;
             if (direction.magnitude > fingerLength)
             {
                 direction = direction.normalized * fingerLength;
@@ -67,9 +52,34 @@ public class FingerTip : MonoBehaviour
             transform.position = Vector3.Lerp(transform.position, targetPosition, fingerSpeed * Time.deltaTime);
         }
 
-        Debug.Log(transform.position.magnitude);
-        Debug.Log(transform.position.magnitude);
+    }
 
+    private void FixedUpdate()
+    {
+        if (isAnchored)
+        {
+            Vector2 mouseDelta = mousePosition - anchoredMousePosition;
+            Debug.Log(mouseDelta);
+            //Vector2 targetPosition = palmRb.position - mouseDelta;
+            Vector2 targetPosition = (Vector2) anchoredPosition - mouseDelta;
+            Vector2 offsetFromAnchor = targetPosition - (Vector2)anchoredPosition;
+            if (offsetFromAnchor.magnitude > fingerLength)
+            {
+                offsetFromAnchor = offsetFromAnchor.normalized * fingerLength;
+            }
+            Vector2 clampedTargetPositon = (Vector2)anchoredPosition + offsetFromAnchor;
+
+            Vector2 palmDisplacement = clampedTargetPositon - palmRb.position;
+
+            //everything above is just calculating the intended displacent, change below to change how the force is set
+            Vector2 force = palmDisplacement * pullSoftness;
+
+            force = Vector2.ClampMagnitude(force, maxPullStrength);
+
+            palmRb.AddForce(force, ForceMode2D.Force);
+            palmRb.velocity *= palmVelocityDampening; //optional drag to prevent buildup
+
+        }
     }
 
 
