a57bd04dbdabaea48927fe9a93bbc44dc59e04ce
vish8sg <tvishnu2008@gmail.com>
2026-01-08 19:43:57 +0800

    Made something that feels good, palm movement needs more damping

--- Assets/Scripts/Tip.cs
+++ Assets/Scripts/Tip.cs
@@ -4,12 +4,73 @@ using UnityEngine;
 
 public class Tip : MonoBehaviour
 {
-    // Update is called once per frame
-    void Update()
+
+    [Tooltip("Increase to increase pulling force")] [SerializeField] float springConstant = 200f;
+    [Tooltip("Increase to prevent un-anchored overshooting")][SerializeField] float damperConstant = 25f;
+    [Tooltip("max pull force that can be exerted on finger rigidbody")] [SerializeField] float maxPullForce = 2000f;
+
+    [SerializeField] Rigidbody2D palmRb;
+
+    Rigidbody2D rb;
+    DistanceJoint2D dJ;
+    TargetJoint2D tJ;
+
+    const float gravity = -9.81f;
+
+    Vector2 prevMousePosition = Vector2.zero;
+    bool isAnchored = false;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        dJ = GetComponent<DistanceJoint2D>();
+        tJ = GetComponent<TargetJoint2D>();
+        tJ.autoConfigureTarget = false;
+        tJ.enabled = false;
+    }
+
+
+    private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            GetComponent<Rigidbody2D>().AddForce(Vector2.right * 100f);
+            isAnchored = true;
+            tJ.enabled = true;
+            tJ.target = rb.position;
         }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            isAnchored = false;
+            tJ.enabled = false;
+        }
+        
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 mousePosition = GetMouseWorldPosition();
+        Vector2 mouseVelocity = (mousePosition - prevMousePosition) / Time.fixedDeltaTime;
+        prevMousePosition = mousePosition;
+
+        Vector2 position = rb.position;
+        Vector2 velocity = rb.velocity;
+
+        Vector2 positionDifference = mousePosition - position;
+        Vector2 velocityDifference = mouseVelocity - velocity;
+
+        Vector2 force = springConstant * positionDifference + damperConstant * velocityDifference;
+        force = Vector2.ClampMagnitude(force, maxPullForce);
+
+        rb.AddForce(force);
+        palmRb.AddForce(-force);
+        
+    }
+
+    Vector2 GetMouseWorldPosition()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        return new Vector2(mouseWorldPosition.x, mouseWorldPosition.y);
     }
 }
